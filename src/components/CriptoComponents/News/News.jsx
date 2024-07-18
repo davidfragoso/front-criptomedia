39581fc41954efca189dec605583a48e3a8cc08f
carloslair24 <92275211+carloslair24@users.noreply.github.com>
2024-07-18 00:53:10 -0500

    exchange

--- src/components/CriptoComponents/News/News.jsx
+++ src/components/CriptoComponents/News/News.jsx
@@ -1,160 +1,248 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import CircularProgress from '@mui/material/CircularProgress';
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {
+  Table, TableBody, TableCell, TableContainer,
+  TableHead, TableRow, Paper, Box, Typography,
+  TablePagination, TextField
+} from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#FFA500', 
-    },
-    secondary: {
-      main: '#005484', 
-    },
-    text: {
-      primary: '#FFFFFF', 
-      secondary: '#B0BEC5' 
-    },
-    background: {
-      paper: '#3B4D5D', 
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+const useStyles = makeStyles({
+  cryptoTableContainer: {
+    margin: '20px',
+    backgroundColor: '#0d1316',
+    color: '#ffffff',
+    padding: '20px',
+    borderRadius: '8px',
+  },
+  cryptoTableTitle: {
+    marginBottom: '10px',
+    color: '#ffffff',
+  },
+  cryptoTableSubtitle: {
+    marginBottom: '20px',
+    color: '#aaaaaa',
+  },
+  cryptoTable: {
+    backgroundColor: '#2b2b2b',
+  },
+  tableHeadCell: {
+    backgroundColor: '#333333',
+    color: '#ffffff',
+  },
+  tableCell: {
+    color: '#ffffff',
+  },
+  tableRowOdd: {
+    backgroundColor: '#2b2b2b',
+  },
+  tableRowEven: {
+    backgroundColor: '#1f1f1f',
+  },
+  pagination: {
+    color: '#ffffff',
+    backgroundColor: '#2b2b2b',
+  },
+  selectIcon: {
+    color: '#ffffff',
+  },
+  searchField: {
+    marginBottom: '20px',
+    backgroundColor: '#333333',
+    borderRadius: '4px',
+  },
+  searchInput: {
+    color: '#ffffff',
+  },
+  link: {
+    color: '#FFA500',
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
     },
   },
-  typography: {
-    button: {
-      color: '#FFA500', 
-    },
+  chartContainer: {
+    marginTop: '40px',
+  },
+  chartTitle: {
+    color: '#ffffff',
+    textAlign: 'center',
+    marginBottom: '20px',
   },
 });
 
-const modalStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '80%',
-  maxHeight: '80%',
-  bgcolor: '#1B242C',
-  boxShadow: 24,
-  p: 4,
-  overflow: 'auto',
-};
-
-const NewsCard = ({ news, handleOpen }) => (
-  <Card
-    sx={{ maxWidth: 345, backgroundColor: '#1B242C', color: 'text.primary', margin: '16px', cursor: 'pointer' }}
-    onClick={() => handleOpen(news)}
-  >
-    {news.urlToImage && (
-      <CardMedia
-        component="img"
-        height="140"
-        image={news.urlToImage}
-        alt={news.title}
-      />
-    )}
-    <CardContent>
-      <Typography variant="h6" component="div" sx={{ color: 'white' }}>
-        {news.title}
-      </Typography>
-      <Typography variant="body2" color="text.secondary">
-        {news.description}
-      </Typography>
-    </CardContent>
-  </Card>
-);
-
-const NewsModal = ({ open, handleClose, news }) => (
-  <Modal
-    open={open}
-    onClose={handleClose}
-    aria-labelledby="modal-modal-title"
-    aria-describedby="modal-modal-description"
-  >
-    <Box sx={modalStyle}>
-      {news.urlToImage && (
-        <CardMedia
-          component="img"
-          height="400"
-          image={news.urlToImage}
-          alt={news.title}
-        />
-      )}
-      <CardContent>
-        <Typography id="modal-modal-title" variant="h4" component="div" sx={{ color: 'white' }}>
-          {news.title}
-        </Typography>
-        <Typography id="modal-modal-description" variant="body2" color="text.secondary" sx={{ mt: 2 }}>
-          {news.content || news.description}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{ mt: 2 }}>
-          <a href={news.url} target="_blank" rel="noopener noreferrer" style={{ color: '#FFA500' }}>
-            Leer más
-          </a>
-        </Typography>
-      </CardContent>
-    </Box>
-  </Modal>
-);
-
-const NFTNewsSection = () => {
-  const [news, setNews] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [selectedNews, setSelectedNews] = useState(null);
-  const [open, setOpen] = useState(false);
+const ExchangesTable = () => {
+  const classes = useStyles();
+  const [exchangesData, setExchangesData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(20);
 
   useEffect(() => {
-    axios.get('https://newsapi.org/v2/everything?q=nft&apiKey=eabbeb3ded5f473380d9d251cef37290')
+    axios.get('https://api.coinpaprika.com/v1/exchanges')
       .then(response => {
-        setNews(response.data.articles);
-        setLoading(false);
+        setExchangesData(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching NFT news:', error);
-        setError(error);
-        setLoading(false);
-      });
+      .catch(error => console.error('Error fetching exchanges data:', error));
   }, []);
 
-  const handleOpen = (news) => {
-    setSelectedNews(news);
-    setOpen(true);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
   };
 
-  const handleClose = () => {
-    setOpen(false);
-    setSelectedNews(null);
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
   };
 
-  if (loading) {
-    return <CircularProgress color="secondary" />;
-  }
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-  if (error) {
-    return <Typography variant="h6" color="error">Error loading NFT news: {error.message}</Typography>;
-  }
+  const filteredData = exchangesData.filter(exchange =>
+    exchange.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    exchange.id.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-  if (news.length === 0) {
-    return <Typography variant="h6" color="text.primary">No NFT news found.</Typography>;
-  }
+  const validData = filteredData.filter(exchange =>
+    exchange.links && exchange.links.website && exchange.links.website[0] &&
+    exchange.quotes && exchange.quotes.USD &&
+    exchange.quotes.USD.reported_volume_24h &&
+    exchange.quotes.USD.reported_volume_7d &&
+    exchange.quotes.USD.reported_volume_30d
+  );
+
+  const displayedData = validData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+
+  const volumeData24h = displayedData.map(exchange => exchange.quotes.USD.reported_volume_24h);
+  const volumeData7d = displayedData.map(exchange => exchange.quotes.USD.reported_volume_7d);
+  const volumeData30d = displayedData.map(exchange => exchange.quotes.USD.reported_volume_30d);
+  const exchangeNames = displayedData.map(exchange => exchange.name);
+
+  const chartData = {
+    labels: exchangeNames,
+    datasets: [
+      {
+        label: 'Volumen 24h (USD)',
+        data: volumeData24h,
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Volumen 7d (USD)',
+        data: volumeData7d,
+        backgroundColor: 'rgba(54, 162, 235, 0.5)',
+      },
+      {
+        label: 'Volumen 30d (USD)',
+        data: volumeData30d,
+        backgroundColor: 'rgba(75, 192, 192, 0.5)',
+      },
+    ],
+  };
+
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Volúmenes de intercambio de exchanges',
+        color: '#ffffff',
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: '#ffffff',
+        },
+      },
+      y: {
+        ticks: {
+          color: '#ffffff',
+        },
+      },
+    },
+  };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container justifyContent="center" sx={{ backgroundColor: '#12161C', padding: 2 }}>
-        {news.map((article, index) => (
-          <NewsCard news={article} key={index} handleOpen={handleOpen} />
-        ))}
-      </Grid>
-      {selectedNews && <NewsModal open={open} handleClose={handleClose} news={selectedNews} />}
-    </ThemeProvider>
+    <Box className={classes.cryptoTableContainer}>
+      <Typography variant="h4" component="h2" className={classes.cryptoTableTitle}>
+        Todos los exchanges
+      </Typography>
+      <Typography variant="subtitle1" className={classes.cryptoTableSubtitle}>
+        Ver una lista completa de los exchanges activos
+      </Typography>
+      <TextField
+        className={classes.searchField}
+        InputProps={{
+          className: classes.searchInput,
+        }}
+        variant="outlined"
+        fullWidth
+        placeholder="Buscar por nombre o ID"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        style={{ backgroundColor: '#1c242d', color: '#ffffff' }}
+      />
+      <TableContainer component={Paper} className={classes.cryptoTable}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.tableHeadCell}>Nombre</TableCell>
+              <TableCell className={classes.tableHeadCell} align="right">Website</TableCell>
+              <TableCell className={classes.tableHeadCell} align="right">Volumen 24h (USD)</TableCell>
+              <TableCell className={classes.tableHeadCell} align="right">Volumen 7d (USD)</TableCell>
+              <TableCell className={classes.tableHeadCell} align="right">Volumen 30d (USD)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {displayedData.map((exchange, index) => (
+              <TableRow key={exchange.id} className={index % 2 === 0 ? classes.tableRowEven : classes.tableRowOdd}>
+                <TableCell className={classes.tableCell}>{exchange.name}</TableCell>
+                <TableCell className={classes.tableCell} align="right">
+                  <a href={exchange.links.website[0]} target="_blank" rel="noopener noreferrer" className={classes.link}>
+                    {exchange.links.website[0]}
+                  </a>
+                </TableCell>
+                <TableCell className={classes.tableCell} align="right">
+                  {exchange.quotes.USD.reported_volume_24h.toFixed(2)}
+                </TableCell>
+                <TableCell className={classes.tableCell} align="right">
+                  {exchange.quotes.USD.reported_volume_7d.toFixed(2)}
+                </TableCell>
+                <TableCell className={classes.tableCell} align="right">
+                  {exchange.quotes.USD.reported_volume_30d.toFixed(2)}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[20]}
+        component="div"
+        count={validData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        className={classes.pagination}
+        classes={{ selectIcon: classes.selectIcon }}
+      />
+      <Box className={classes.chartContainer}>
+        <Typography variant="h5" className={classes.chartTitle}>
+          Gráficas de Volúmenes de Intercambio
+        </Typography>
+        <Bar data={chartData} options={chartOptions} />
+      </Box>
+    </Box>
   );
 };
 
-export default NFTNewsSection;
+export default ExchangesTable;
