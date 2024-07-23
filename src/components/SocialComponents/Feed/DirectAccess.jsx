6e2e73c567b9f2f04dd77b5a735a861d29b43ba8
Gael Ulises Martínez López <gaelml22@gmail.com>
2024-07-23 16:07:05 -0500

    Updated post details

--- src/components/SocialComponents/Feed/DirectAccess.jsx
+++ src/components/SocialComponents/Feed/DirectAccess.jsx
@@ -49,7 +49,7 @@ const DirectAccess = () => {
         <span>Publicación</span>
       </div>
       <div style={styles.divider}></div>
-           <div style={styles.item}>
+          <div style={styles.item}>
         <ChatIcon style={styles.icon} />
         <span>Chat</span>
       </div>
